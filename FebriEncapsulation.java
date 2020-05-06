bc7e170394f3cac93cbcb679f04beed2ccc77180
Febri Hidayan <febrihidayan20@gmail.com>
2020-05-06 13:10:58 +0700

    Penambahan setter dan getter

--- FebriEncapsulation.java
+++ FebriEncapsulation.java
@@ -1,6 +1,8 @@
 class FebriEncapsulation {
 
     private double speed;
+    private double jump;
+    private String messages;
 
     public void setSpeed(double newSpeed)
     {
@@ -10,6 +12,26 @@ class FebriEncapsulation {
     public double getSpeed()
     {
         return speed;
+    }
+
+    public void setJump(double newJump)
+    {
+        this.jump = newJump;
+    }
+
+    public double getJump()
+    {
+        return jump;
+    }
+
+    public void setMessages(String newMessages)
+    {
+        this.messages = newMessages;
+    }
+
+    public String getMessages()
+    {
+        return messages;
     }
     
 }
--- FebriGames.java
+++ FebriGames.java
@@ -1,9 +1,24 @@
 public class FebriGames {
 
     public static void main(String[] args) {
+
         FebriEncapsulation myObj = new FebriEncapsulation();
+
         myObj.setSpeed(100);
+        myObj.setMessages("Mobilku cukup lambat");
         System.out.println(myObj.getSpeed());
+        System.out.println(myObj.getMessages());
+        
+        myObj.setMessages("Saya ingin menambahkannya");
+        myObj.setSpeed(99999);
+        System.out.println(myObj.getMessages());
+        System.out.println(myObj.getSpeed());
+        
+        myObj.setMessages("Saatnya lompat");
+        myObj.setJump(9991);
+        System.out.println(myObj.getMessages());
+        System.out.println("Kekuatan lompat " + myObj.getJump());
+
     }
 
 }
